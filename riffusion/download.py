6477a670d5186834a63d2f1eb554b5fdf026ae51
Dylan <dylanpdx@gmail.com>
2023-02-21 20:14:28 +0000

    Fixed files downloading during inference

--- riffusion/download.py
+++ riffusion/download.py
@@ -1,5 +1,6 @@
 import torch
 from riffusion_pipeline import RiffusionPipeline
+from huggingface_hub import hf_hub_download
 def download_model():
     model = RiffusionPipeline.from_pretrained(
         "riffusion/riffusion-model-v1",
@@ -7,6 +8,9 @@ def download_model():
         torch_dtype=torch.float16,
         # Disable the NSFW filter, causes incorrect false positives
         safety_checker=lambda images, **kwargs: (images, False),
+    )
+    unet_file = hf_hub_download(
+        "riffusion/riffusion-model-v1", filename="unet_traced.pt", subfolder="unet_traced"
     )
 
 if __name__ == "__main__":
